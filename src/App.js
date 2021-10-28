138751c5b604c016e307348b7e9e8ded98019e64
Cauã Muliterno Marinho Campos <cauacampos05@gmail.com>
2021-10-28 15:48:15 -0300

    feat: navbar criada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Login from "./Pages/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div>
         <Switch>
           <Route path="/" component={Login} />

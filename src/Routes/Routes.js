9a55abf51298ecadde45c8a28ca2aa842316c8a9
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-10-29 03:49:07 -0300

    refactor: Routes.js
    Feito ajuste na rota de login.

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import CardObjective from '../CardObjective';
-=======
->>>>>>> 138751c5b604c016e307348b7e9e8ded98019e64
 import Login from '../Pages/Login/Login';
 
 function Routes() {
   return (
     <div>
       <Switch>
-<<<<<<< HEAD
         <Route path="/" exact component={Login} />
         <Route path="/objective" component={CardObjective} />
-=======
-        <Route path="/" component={Login} />
->>>>>>> 138751c5b604c016e307348b7e9e8ded98019e64
       </Switch>
     </div>
   )

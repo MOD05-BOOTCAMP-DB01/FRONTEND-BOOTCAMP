aa1edc6e3d203a47387af97a79137b8db4c49e68
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-10-29 03:50:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
 import Routes from "./Routes/Routes";
-
-=======
-import { Switch, Route } from "react-router-dom";
-import Login from "./Pages/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
->>>>>>> 138751c5b604c016e307348b7e9e8ded98019e64
 
 function App() {
   return (

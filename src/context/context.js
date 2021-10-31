d0d190a7959153b19f6011cbcd45f1bf85f3f24b
thaynar96 <thaynarbo@gmail.com>
2021-10-31 14:26:29 -0300

    feat(context): implement context

---
+++ src/context/context.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from "react";
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+  //  funções e estados globais
+  const [value, setValue] = useState(0);
+
+  const completeTask = () => {
+    increment();
+    // escrever função para enviar um update por endpoint para o banco
+  };
+  const increment = () =>
+    setValue((prevState) => (prevState >= 100 ? 0 : prevState + 20));
+  return (
+    <AppContext.Provider value={{ completeTask }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// hook customizado com context
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "./context/context";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

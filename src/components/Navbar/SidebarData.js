0e10e420143095dde4113b9efb90eae24d575c2a
thaynar96 <thaynarbo@gmail.com>
2021-10-31 14:47:56 -0300

    chore(sidebar): change path to objectives

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { BiTask } from "react-icons/bi";
-import { GiArchiveRegister } from "react-icons/gi"
+import { GiArchiveRegister } from "react-icons/gi";
 
 export const SidebarData = [
-    {
-        title: "Login",
-        path: "/",
-        icon: <AiFillHome />,
-        cName: "nav-text"
-    },
-    {
-        title: "Cadastro",
-        path: "/register",
-        icon: <GiArchiveRegister />,
-        cName: "nav-text"
-    },
-    {
-        title: "Objetivos",
-        path: "/objective",
-        icon: <BiTask />,
-        cName: "nav-text"
-    }
+  {
+    title: "Login",
+    path: "/",
+    icon: <AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Cadastro",
+    path: "/register",
+    icon: <GiArchiveRegister />,
+    cName: "nav-text",
+  },
+  {
+    title: "Objetivos",
+    path: "/objectives",
+    icon: <BiTask />,
+    cName: "nav-text",
+  },
 ];

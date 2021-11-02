c10fca8fdab82abf56dfbca1bd216540fdfbbf88
thaynarbo <thaynarbo@gmail.com>
2021-11-01 22:00:37 -0300

    feat(api): implement api endpoint and jwtHandler

---
+++ src/Api/Api.js
@@ -0,0 +1,60 @@
+import { JwtHandler } from "../jwt-handler/JwtHandler";
+export const Api = {
+  baseUrl: "http://localhost:3000",
+
+  loginUrl: () => `${Api.baseUrl}/auth/signin`,
+
+  // OBJECTIVES
+  readAllObjectives: () => `${Api.baseUrl}/objectives`,
+  readObjectivesById: (id) => `${Api.baseUrl}/objectives/${id}`,
+  createObjectiveUrl: () => Api.baseUrl + "/objectives",
+  updateObjectiveUrl: (id) => `${Api.baseUrl}/objectives/${id}`,
+  deleteObjectiveUrl: (id) => `${Api.baseUrl}/objectives/${id}`,
+
+  // KRS
+  readAllKrsUrl: () => `${Api.baseUrl}/key-results`,
+  readKrsbyIdUrl: (id) => `${Api.baseUrl}/key-results/${id}`,
+  createKrsUrl: () => `${Api.baseUrl}/key-results`,
+  deleteKrsUrl: (id) => `${Api.baseUrl}/key-results/${id}`,
+  updateKrsUrl: (id) => `${Api.baseUrl}/key-results/${id}`,
+
+  // USERS
+  createUserUrl: () => `${Api.baseUrl}/auth/signup`,
+  readAllUsers: () => `${Api.baseUrl}/users`,
+  readAllUsersbyId: (id) => `${Api.baseUrl}/users/${id}`,
+
+  authHeader: () => ({
+    Authorization: "Bearer " + JwtHandler.getJwt(),
+  }),
+
+  buildApiGetRequest: (url, auth) =>
+    fetch(url, {
+      method: "GET",
+      headers: auth ? new Headers(Api.authHeader()) : undefined,
+    }),
+
+  buildApiPostRequest: (url, body, auth) =>
+    fetch(url, {
+      method: "POST",
+      headers: new Headers({
+        "content-type": "application/json",
+        ...(auth ? Api.authHeader() : {}),
+      }),
+      body: JSON.stringify(body),
+    }),
+
+  buildApiPatchRequest: (url, body, auth) =>
+    fetch(url, {
+      method: "PATCH",
+      headers: new Headers({
+        "content-type": "application/json",
+        ...(auth ? Api.authHeader() : {}),
+      }),
+      body: JSON.stringify(body),
+    }),
+  buildApiDeleteRequest: (url, auth) =>
+    fetch(url, {
+      method: "DELETE",
+      headers: auth ? Api.authHeader() : {},
+    }),
+};

3f9dd6e495bfd2250383748c296d5397fb3c950e
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-11-03 19:50:47 -0300

    refactor: CardObjective
    Feito uma mudança para deixar o card mais componentizado.

--- src/components/CardObjective/CardObjective.js
+++ src/components/CardObjective/CardObjective.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { BiCommentDots } from "react-icons/bi";
-import { MdOutlineLibraryAdd } from "react-icons/md";
+import React, { useEffect, useState } from 'react'
+
 
 //CSS
-import "./cardObjective.css";
-import { cks } from "../../Api/mock/data";
-import data from "../../Api/mock/data";
-import { Api } from "../../Api/Api";
-
+import './cardObjective.css'
+import { cks } from '../../Api/mock/data'
+import data from '../../Api/mock/data'
+import { Api } from '../../Api/Api'
+import CardKr from '../CardKeyResult/CardKr'
+import CardCk from '../CardCheckin/CardCk'
 
 export default function CardObjective() {
   // const objective = {
@@ -21,39 +21,51 @@ export default function CardObjective() {
   // };
 
   const [objective, setObjective] = useState(undefined)
+  const [krs, setKrs] = useState(undefined)
+  const [cks, setCks] = useState(undefined)
 
-  const [changeView, setChangeView] = useState(true);
+  const [changeView, setChangeView] = useState(true)
 
-  const id = "67615f4b-734e-4fce-8bf2-33d44160b864"
+  const id = '67615f4b-734e-4fce-8bf2-33d44160b864'
 
   useEffect(() => {
     const loadObjective = async () => {
-      const response = await Api.buildApiGetRequest(Api.readObjectivesById(id),true)
+      const response = await Api.buildApiGetRequest(
+        Api.readObjectivesById(id),
+        true,
+      )
       const results = await response.json()
-        setObjective(results.objective)
+      setObjective(results.objective)
+      setKrs(results.objective.key_results)
+      // setCks(results.objective.key_results.cks)
     }
     loadObjective()
-  }, []);
-  console.log(objective)
+  }, [])
+  console.log('Objective', objective)
+  // console.log("objective.key_results.cks",objective.key_results.cks)
+
+  // useEffect(() => {
+  //   const loadKr = async () => {
+  //     const response = await Api.buildApiGetRequest(Api.readKrsbyIdUrl("4626aa14-8a6d-4ea2-9375-65cc06b162f1"),true)
+  //     const results = await response.json()
+  //       setKr(results)
+  //       setCks(results.keyResult.cks)
+  //   }
+  //   loadKr()
+  // }, []);
+  console.log('kr=', krs)
+  console.log('Cks=', cks)
+
   if (!objective) {
+    return <h3>Loading..</h3>
+  }
+  if (!krs) {
     return <h3>Loading..</h3>
   }
 
   const alterChangeView = () => {
-    setChangeView(!changeView);
-  };
-
-  const statusColor = (status) => {
-    if (status === "Em andamento") {
-      return <h4 className="yellow">Em andamento</h4>;
-    } else if (status === "Concluído") {
-      return <h4 className="gree">Concluído</h4>;
-    } else if (status === "Atraso crítico") {
-      return <h4 className="red">Atraso crítico</h4>;
-    } else {
-      return;
-    }
-  };
+    setChangeView(!changeView)
+  }
 
   return (
     // Objective
@@ -63,7 +75,7 @@ export default function CardObjective() {
           <h3>{objective.unity}</h3>
         </div>
         <button className="changeView" onClick={alterChangeView}>
-          {changeView ? "Detalhes" : "Ver Status"}
+          {changeView ? 'Detalhes' : 'Ver Status'}
         </button>
         <div className="objective">
           <h3>{objective.area}</h3>
@@ -73,131 +85,17 @@ export default function CardObjective() {
 
         <div>
           <h4>
-            {objective.initial_date} {"-"} {objective.end_date}
+            {objective.initial_date} {'-'} {objective.end_date}
           </h4>
         </div>
       </div>
       {/* Conditional render */}
-      {!changeView ? (
-
-      // KR 
-        <div className="area-kr">
-          <div className="kr-header">
-              <div className="kr-title-header">
-                <h3>Titulo KR</h3>
-                <MdOutlineLibraryAdd className="icon-add" />
-              </div>
-
-              <div className="kr-owner-header">
-                <h3>Dono KR</h3>
-              </div>
-
-              <div className="kr-comment-header">
-                <h3>Comentário</h3>
-              </div>
-
-              <div className="kr-type-header">
-                <h3>Tipo KR</h3>
-              </div>
-
-              <div className="kr-frequency-header">
-                <h3>Frequência Medição</h3>
-              </div>
-
-              <div className="kr-classification-header">
-                <h3>Classificação</h3>
-              </div>
-
-              <div className="kr-vlInitial-header">
-                <h3>Vl. Inicial</h3>
-              </div>
-
-              <div className="kr-vlGoal-header">
-                <h3>Vl. Meta</h3>
-              </div>
-
-              <div className="kr-done-header">
-                <h3>Concluído</h3>
-              </div>
-
-            </div>
-            
-          {/*Body KR  */}
-          {objective.key_results.map((kr,i) => (
-            <div className="kr-items" key={`kr-${i}`}>
-              <div className="kr-title-items">
-                <h4>{kr.key_result}</h4>
-              </div>
-
-              <div className="kr-owner-items">
-                <h4>{kr.owner.username}</h4>
-              </div>
-
-              <div className="kr-comment-items">
-                <p>{kr.comment}</p>
-                <BiCommentDots className="icon-comment" />
-              </div>
-
-              <div className="kr-type-items">
-                <h4>{kr.type}</h4>
-              </div>
-
-              <div className="kr-frequency-items">
-                <h4>{kr.frequency}</h4>
-              </div>
-
-              <div className="kr-classification-items">
-                <h4>{kr.rating}</h4>
-              </div>
-
-              <div className="kr-vlInitial-items">
-                <h4>{kr.initial_value}</h4>
-              </div>
-
-              <div className="kr-vlGoal-items">
-                <h4>{kr.goal_value}</h4>
-              </div>
-              <div className="kr-done-items">
-                <input type="checkbox" />
-              </div>
-            </div>
-          ))}
-          
-        </div>
-      ) : (
-        /* Checking */
-        // repeat area title
-        <div className="area-ck">
-          <div className="kr-header-ck">
-            <div className="kr-header">
-              <h3>Titulo KR</h3>
-              <MdOutlineLibraryAdd />
-            </div>
-            {objective.key_results.map((kr,i) => (
-              <div key={`kr-ch-${i}`} className="kr-title-items-ck">
-                <h4>{kr.key_result}</h4>
-              </div>
-              
-            ))}
-          </div>
-
-          {cks.map((ck,i) => (
-            <div key={`ck-${i}`} className="ck">
-              <div className="ck-header">
-                <h3>{ck.date}</h3>
-              </div>
-              {ck.status.map((st) => (
-                <div className="ck-items">
-                  {statusColor(st)}
-                </div>
-              ))}
-              
-            </div>
-          ))}
-
-          
-        </div>
-      )}
+      {!changeView 
+      ?
+        <CardKr krs={objective.key_results}/> 
+      :
+        <CardCk krs={objective.key_results}/>
+      } 
     </div>
-  );
+  )
 }

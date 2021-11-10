283ae3274aa5b003f1439f591735ae6eac032571
Cauã Muliterno Marinho Campos <cauacampos05@gmail.com>
2021-11-10 19:44:18 -0300

    Revert "feat(Navbar): logout component created"
    This reverts commit ea14b39a8de42d88eeb6de6d0800da1f21d37e3f.

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import CardObjective from "../components/CardObjective/CardObjective";
-import Logout from "../components/Logout/Logout";
 import Objective from "../components/Objective/Objective";
 import Home from "../Pages/Home/Home";
 import CreateObjective from "../Pages/Objective/CreateObjective/CreateObjective";
@@ -12,7 +11,6 @@ function Routes() {
     <div>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/logout" exact component={Logout} />
         <Route path="/objective/:id" component={CardObjective} />
         <Route path="/objective" component={CreateObjective} />
         <Route path="/objectives" component={Objective} />

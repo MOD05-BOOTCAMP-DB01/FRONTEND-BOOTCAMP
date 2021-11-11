cfadb621f67e44ce43382dc26d4ff58e4768fa21
Cauã Muliterno Marinho Campos <cauacampos05@gmail.com>
2021-11-10 21:39:33 -0300

    Logout

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import CardObjective from "../components/CardObjective/CardObjective";
+import Logout from "../components/Logout/Logout";
 import Objective from "../components/Objective/Objective";
 import Home from "../Pages/Home/Home";
 import CreateObjective from "../Pages/Objective/CreateObjective/CreateObjective";
@@ -11,6 +12,7 @@ function Routes() {
     <div>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/logout" exact component={Logout} />
         <Route path="/objective/:id" component={CardObjective} />
         <Route path="/objective" component={CreateObjective} />
         <Route path="/objectives" component={Objective} />

0292d82abbd04f01320cc1947586a50f1a2a102f
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-11-11 00:59:42 -0300

    feat: viewMore
    Feito o ver mais e ajustes no layout dos cards do key result.

--- src/components/CardKeyResult/ViewMoreKr.js
+++ src/components/CardKeyResult/ViewMoreKr.js
@@ -1,12 +1,40 @@
 import React from 'react'
 
+import { BiCommentDots } from 'react-icons/bi'
+
 import './viewMoreKr.css'
 
 export default function ViewMoreKr({kr}) {
+  console.log("kr viewMoreKr", kr)
   return (
-    <div className="viewMoreKr">
-      <div className="viewMoreKr-comment">
-        <h3>{kr.comment}</h3>
+    <div className="area-viewMoreKr">
+      <div className="viewMoreKr">
+
+        <div className="viewMoreKr-comment">
+            <p>{kr.comment}</p>
+            <BiCommentDots className="icon-comment" />
+        </div>
+
+        <div className="viewMoreKr-frequency">
+          <h3>{kr.frequency}</h3>
+        </div>
+
+        <div className="viewMoreKr-initialValue">
+          <h3>{kr.initial_value}</h3>
+        </div>
+
+        <div className="viewMoreKr-goalValue">
+          <h3>{kr.goal_value}</h3>
+        </div>
+
+        <div className="viewMoreKr-checkin">
+          <h3>Check-in</h3>
+        </div>
+
+        <div className="viewMoreKr-checkin">
+          <input type="checkbox"></input>
+        </div>
+
       </div>
     </div>
   )

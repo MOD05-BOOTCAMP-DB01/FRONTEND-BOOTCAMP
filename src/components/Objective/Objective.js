84009a7e89ddb832442ae77da5f81c357fe1b901
thaynarbo <thaynarbo@gmail.com>
2021-11-12 00:26:36 -0300

    fix(objective): add title and correct language

--- src/components/Objective/Objective.js
+++ src/components/Objective/Objective.js
@@ -25,21 +25,24 @@ const Objective = () => {
   console.log(objectives)
   return (
     <div className="objective-container">
+    <div className="objective-container-heading">
+    <h1>Objetivos</h1>
+    </div>
       {objectives.map((objective) => {
         const { objective: name, initial_date, id, type,owner } = objective;
         return (
           <Link to={`/objective/${id}`}>
             <div className="objective-card" key={id}>
               <h3>
-                <span className="objective-info">title:</span>
+                <span className="objective-info">Titulo:</span>
                 {name}
               </h3>
               <h4>
-                <span className="objective-info">date:</span>
+                <span className="objective-info">Data:</span>
                 {initial_date}
               </h4>
               <h4>
-                <span className="objective-info">dono:</span>
+                <span className="objective-info">Dono:</span>
                 {owner.username}
               </h4>
 

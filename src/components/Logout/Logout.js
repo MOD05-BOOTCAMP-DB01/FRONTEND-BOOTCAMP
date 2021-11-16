68e0a01df928697db8ea45c291d98433ff07a382
thaynarbo <thaynarbo@gmail.com>
2021-11-15 23:27:12 -0300

    feat: add admin sidebar data and implement 404 page

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import { Redirect } from "react-router";
+import { useGlobalContext } from "../../context/context";
 import { JwtHandler } from "../../jwt-handler/JwtHandler";
 
 export default function Logout() {
+    const {setLoggedUser} = useGlobalContext();
     useEffect(() => {
         JwtHandler.clearJwt();
+        setLoggedUser('')
     });
 
     return <Redirect to="/"/>;

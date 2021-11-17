c2782bf332cefbcabae4f417f70e0533a7c9083d
Wendeel Lima <wendeel.admpar@gmail.com>
2021-11-17 19:39:50 -0400

    feat: botão modal implement

--- src/components/ModalAcess/ModalAcess.js
+++ src/components/ModalAcess/ModalAcess.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
+import { FcInfo } from "react-icons/fc";
 
 function ModalAcess({ children }) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <div>
-      <button onClick={() => setIsModalVisible(true)}>open</button>
+      <button onClick={() => setIsModalVisible(true)}>
+        <FcInfo />
+      </button>
       {isModalVisible ? (
         <Modal onClose={() => setIsModalVisible(false)}>{children}</Modal>
       ) : null}

274744b85b780d8a27a32f43b90609835ad64b68
Wendeel Lima <wendeel.admpar@gmail.com>
2021-11-22 19:50:51 -0400

    fix (correção handlesubmit

--- src/Pages/Team/CreateTeam.js
+++ src/Pages/Team/CreateTeam.js
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
-import { Api } from '../../Api/Api';
-import {toast } from 'react-toastify';
-import Button from '../../components/Button/Button';
+import React, { useState } from "react";
+import { Api } from "../../Api/Api";
+import { toast } from "react-toastify";
+import Button from "../../components/Button/Button";
 
 function CreateTeam() {
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const team = e.target.team.value;
+    const team = e.target.team.value;
 
-        const payload = {
-            team,
-        };
+    const payload = {
+      team,
+    };
 
-        const response = await Api.buildApiPostRequest(
-            Api.readAllTeams,
-            payload,
-            true,
-            console.log(payload),
-            console.log(e.target.team.value),
-            console.log(response),
-        );
+    console.log(team);
 
-        const body = await response.json();
+    const response = await Api.buildApiPostRequest(
+      Api.readAllTeams,
+      payload,
+      true
+    );
 
-        if (response.status === 201) {
-            toast.success('Time cadastrado com sucesso!', {theme: "dark",position: toast.POSITION.TOP_CENTER,
-            });
-        }
+    const body = await response.json();
 
+    if (response.status === 201) {
+      toast.success("Time cadastrado com sucesso!", {
+        theme: "dark",
+        position: toast.POSITION.TOP_CENTER,
+      });
     }
+  };
 
-    return (
-        <div>
-            <form className="team_form">
-                <div className="team_form_input">
-                    <input id="team" type="text" placeholder="Nome do time" name="team" />
-                </div>
-
-                <div className="team_button">
-                    <Button onClick={handleSubmit} type="submit">Criar</Button>
-                </div>
-            </form>
+  return (
+    <div>
+      <form className="team_form" onSubmit={handleSubmit}>
+        <div className="team_form_input">
+          <input id="team" type="text" placeholder="Nome do time" name="team" />
         </div>
-    )
+
+        <div className="team_button">
+          <Button type="submit">Criar</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default CreateTeam
+export default CreateTeam;
